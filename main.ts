7faa757cfdaa105a4b1efd36da2c472f9654f4d9
cakepower <32028127+cakepower@users.noreply.github.com>
2021-12-23 08:20:04 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -13,3 +13,12 @@ input.onButtonPressed(Button.B, function () {
 input.onPinPressed(TouchPin.P1, function () {
     pins.servoWritePin(AnalogPin.P14, 180)
 })
+basic.forever(function () {
+    if (!(input.pinIsPressed(TouchPin.P1))) {
+        pins.servoWritePin(AnalogPin.P14, 90)
+    } else if (!(input.pinIsPressed(TouchPin.P2))) {
+        pins.servoWritePin(AnalogPin.P14, 90)
+    } else {
+    	
+    }
+})
